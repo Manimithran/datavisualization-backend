132995da8dc4c46c73f5f028057f465ba8c95d00
MANI MITHRAN K R <144508689+Manimithran@users.noreply.github.com>
2024-07-13 13:38:45 +0530

    Update DBconnect.js

--- DBconnect.js
+++ DBconnect.js
@@ -1,12 +1,18 @@
 const mongoose = require("mongoose");
+require('dotenv').config();
 
-function DBConnect(){
-    mongoose.connect(process.env.DbConnect)
-    .then((conn) => {
-        console.log("Connected to DB");
-    }).catch((error) => {
-        console.error(error);
+
+function DBConnect() {
+    mongoose.connect(process.env.DbConnect, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
     })
+    .then(() => {
+        console.log("Connected to DB");
+    })
+    .catch((error) => {
+        console.error("DB connection error: ", error);
+    });
 }
 
 module.exports = DBConnect;
