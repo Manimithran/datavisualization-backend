c0e024cd3db1fce299b6713b327e90f2558972df
MANI MITHRAN K R <144508689+Manimithran@users.noreply.github.com>
2024-07-14 19:33:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const DBConnect = require("./DBconnect");
 const bodyParser = require('body-parser');
-const DataModel =require("./schema")
+const DataModel = require("./schema");
 const dataRouter = require('./categorize');
+
 dotenv.config();
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -13,30 +15,29 @@ app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
 app.use(cors());
+
 DBConnect();
 
-const corsoption= {
-  origin : "http://localhost:3000"
-}
+const corsOptions = {
+    origin: "http://localhost:3000"
+};
 
-app.use(cors(corsoption))
+app.use(cors(corsOptions));
 
 app.post('/populate', async (req, res) => {
     try {
-      const data = req.body; // Expecting an array of objects
-      if (!Array.isArray(data)) {
-        return res.status(400).json({ message: 'Data should be an array of objects' });
-      }
-      await DataModel.insertMany(data);
-      res.status(201).json({ message: 'Data successfully populated' });
+        const data = req.body; // Expecting an array of objects
+        if (!Array.isArray(data)) {
+            return res.status(400).json({ message: 'Data should be an array of objects' });
+        }
+        await DataModel.insertMany(data);
+        res.status(201).json({ message: 'Data successfully populated' });
     } catch (error) {
-      res.status(500).json({ message: error.message });
+        res.status(500).json({ message: error.message });
     }
-  });
+});
 
-  app.use('/api', dataRouter); // Use the router
-
-
+app.use('/api', dataRouter); // Use the router
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
